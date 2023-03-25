bdbabb655486c70d8e2c5c05a41bd3a0607ac128
lazaroah <lazaroah02@gmail.com>
2023-03-25 19:09:09 -0400

    fixed the auto scroll of recomended products

--- src/components/ProductsRecomended/index.js
+++ src/components/ProductsRecomended/index.js
@@ -1,51 +1,67 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import "./index.css";
 import Card from '../Card';
 import rightArrow from '../../assets/right-arrow-alt-regular-24.png'
 import leftArrow from '../../assets/left-arrow-alt-regular-24.png'
 
 export default function ProductsRecomended(){
-    const items = [
-        {id:1, name:"Primero", precio: "100", foto: ""},
-        {id:2, name:"Segundo", precio: "100", foto: ""},
-        {id:3, name:"Tercero", precio: "100", foto: ""},
-        {id:4, name:"Cuarto", precio: "100", foto: ""},
-        {id:5, name:"Quinto", precio: "100", foto: ""},
-        {id:6, name:"Ultimo", precio: "100", foto: ""},
-    ]
+    const [items, setItems] = useState([])
+    const scrollRef = useRef()
 
+    useEffect(() => {
+        setItems ([
+            {id:1, name:"Primero", precio: "100", foto: ""},
+            {id:2, name:"Segundo", precio: "100", foto: ""},
+            {id:3, name:"Tercero", precio: "100", foto: ""},
+            {id:4, name:"Cuarto", precio: "100", foto: ""},
+            {id:5, name:"Quinto", precio: "100", foto: ""},
+            {id:6, name:"Ultimo", precio: "100", foto: ""},
+        ])
+    },[])
+    
     let cont = 0
-    setTimeout(() => seeNextProduct(), 10000)
+    if(scrollRef.current != undefined){
+        setTimeout(() => seeNextProduct(), 10000)
+        console.log('a')
+    }
     //handle previous item
     function seePreviousProduct(){
+        let increment = scrollRef.current.scrollWidth / items.length
         if(cont == 0){   //compruebo si se encuentra en el primer item y lo redirigo al ultimo
             cont = items.length
             return seePreviousProduct()
         }
         if(cont > 0){
             cont -= 1
-            let item = document.getElementById(items[cont].id)
-            item.scrollIntoView({behavior:"smooth",block:"center", inline:"center"})
+            scrollRef.current.scrollTo({
+                left:cont*increment, 
+                top:0, 
+                behavior:"smooth"
+              })
             setTimeout(() => seeNextProduct(), 10000)
         }  
     }   
     //handle next item   
     function seeNextProduct(){
+        let increment = scrollRef.current.scrollWidth / items.length
         if(cont == items.length-1){  //compruebo si se encuentra en el ultimo item y lo redirigo al primero
             cont = -1
             return seeNextProduct()
         }
-        if(cont < items.length-1){
+        if(cont < items.length){
             cont += 1
-            let item = document.getElementById(items[cont].id)
-            item.scrollIntoView({behavior:"smooth",block:"center", inline:"center"})
+            scrollRef.current.scrollTo({
+                left:cont*increment, 
+                top:0, 
+                behavior:"smooth"
+              })
             setTimeout(() => seeNextProduct(), 10000)  
         }
     }
     return(
         <div>
             <h5 class = "title">Productos Recomendados</h5>
-            <section className = "ProductsRecomendedContainer">
+            <section className = "ProductsRecomendedContainer" ref = {scrollRef}>
                 {items.map((item) => <Card {...item}/>)}
             </section>
 
