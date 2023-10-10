a87e2b288a15df67a04fe675d8081607cb74315a
lazaroah <lazaroah02@gmail.com>
2023-10-10 18:19:06 -0400

    version for deploy on render

--- src/context/ProductsCartContext/index.js
+++ src/context/ProductsCartContext/index.js
@@ -3,11 +3,7 @@ import React, {useState} from 'react'
 const Context = React.createContext({})
 
 export function ProductsCartContextProvider({children}){
-    const [productsCart, setProductCart] = useState([
-        {id: 1, name: 'Pulover', price: 100, cantidad: 1, subtotal: 100},
-        {id: 5, name: 'Perro', price: 10000, cantidad: 1, subtotal: 10880},
-        {id: 3, name: 'Pulogr', price: 1044440, cantidad: 1, subtotal: 100},
-    ])
+    const [productsCart, setProductCart] = useState([])
     return (<Context.Provider value = {{productsCart, setProductCart}}>
         {children}
         </Context.Provider>)
